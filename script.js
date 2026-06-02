f12350b1968b5271452e9283cad67cedf5dff3f5
NilonSedocode <156731859+Gethub911809@users.noreply.github.com>
2026-06-02 03:05:42 +0000

    Massive overhaul to website

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function openPage(pageName) {
-  // Hide all elements with class="tabcontent" by default */
+  // Hide all elements with class="tabcontent" by default
   var i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
@@ -12,9 +12,14 @@ function openPage(pageName) {
     tablinks[i].style.backgroundColor = "";
   }
 
-  // Show the specific tab content
-  document.getElementById(pageName).style.display = "block";
+  var page = document.getElementById(pageName);
+  if (page) {
+    page.style.display = "block";
+  }
 }
 
-// Get the element with id="defaultOpen" and click on it
-document.getElementById("defaultOpen").click();
+// If the page has a defaultOpen tab, click it.
+var defaultOpen = document.getElementById("defaultOpen");
+if (defaultOpen) {
+  defaultOpen.click();
+}
